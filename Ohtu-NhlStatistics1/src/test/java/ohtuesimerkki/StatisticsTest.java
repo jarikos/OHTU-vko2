54e9ca69682bd9b2fbe8d4cd45af840df2637f0f
jarikos <jari.m.koskinen@helsinki.fi>
2012-03-25 19:51:27 +0300

    new file:   src/test/java/ohtuesimerkki/StatisticsTest.java

---
+++ Ohtu-NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JKosk2
+ */
+public class StatisticsTest {
+    Statistics stats;
+    ReaderStub stub;
+    
+    @Before
+    public void setUp() {
+        stub = new ReaderStub();
+        stats = new Statistics(stub);
+    }
+    
+    @Test
+    public void statisticsOK(){
+
+        assertEquals("[Gretzky              EDM 35 + 89 = 124]", stats.topScorers(0).toString());
+    }
+    
+    @Test
+    public void statisticsTeam(){
+
+        assertEquals("[Lemieux              PIT 45 + 54 = 99]", stats.team("PIT").toString());
+    }
+} 
